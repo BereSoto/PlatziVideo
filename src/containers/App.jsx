9bdc294ef1ec1eda0009c0972baf069f142ff2df
BereSoto <beresoto.f@gmail.com>
2019-10-11 12:45:15 -0500

    cambio ejercicido

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categoria from '../components/Categoria';
